22de621d12f46b808c8071f10022b69ff33c3420
Artem Kotelevych <artemiykotelevych@gmail.com>
2018-11-16 23:52:56 +0200

    initial commit

---
+++ src/main/java/org/khai/learning/api/A.java
@@ -0,0 +1,10 @@
+package org.khai.learning.api;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class A {
+    public A() {
+        System.out.println("############");
+    }
+}
